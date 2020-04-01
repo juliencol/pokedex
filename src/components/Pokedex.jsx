3f8f8e8898af70aee14cd4ea28d4df8222c70145
Julien Colombain <julien@colombain.com>
2020-04-01 19:27:13 +0200

    added masta and don paco

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -15,6 +15,8 @@ class Pokedex extends Component {
         <Pokecard id={6} name="Jigglypuff" type="normal" baseExperience={95} image="https://img.pokemondb.net/artwork/large/jigglypuff.jpg" />
         <Pokecard id={7} name="Gengar" type="poison" baseExperience={225} image="https://img.pokemondb.net/artwork/large/gengar.jpg" />
         <Pokecard id={9} name="Eevee" type="normal" baseExperience={65} image="https://upload.wikimedia.org/wikipedia/en/thumb/a/a9/Pok%C3%A9mon_Eevee_art.png/220px-Pok%C3%A9mon_Eevee_art.png" />
+        <Pokecard id={9} name="Masta" type="tismé" baseExperience={1420} image="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/90299063_1291465051133280_7802020968943058944_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_ohc=aoEFWwQI6i8AX8wJzdS&_nc_ht=scontent-cdg2-1.xx&oh=d85bf49786434cbafdf99cbee394dd2e&oe=5EABE180" />
+        <Pokecard id={9} name="Don Paco" type="Docteur" baseExperience={4} image="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/83598907_278356476472462_7934220839703019520_o.jpg?_nc_cat=107&_nc_sid=7aed08&_nc_ohc=WLqnGzACeYkAX9bQzM2&_nc_ht=scontent-cdg2-1.xx&oh=0a4d3c5fe504a29b5f7bc7ae97748fa2&oe=5EAAD4F6" />
       </div>
     )
   }
