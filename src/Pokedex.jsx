24bbc36740d31a70af310c60eeab1a019682334f
Julien Colombain <julien@colombain.com>
2020-04-01 15:25:19 +0200

    init pokedex

---
+++ src/Pokedex.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Pokedex extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pokedex</h1>
+        <p>This is my pokedex</p>
+      </div>
+    )
+  }
+}
+
+export default Pokedex;
